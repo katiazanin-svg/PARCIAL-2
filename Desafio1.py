b94f650a2342d9ca51e51c137da58ef4a2ab958b
katiazanin-svg <katia.zanin@escola.pr.gov.br>
2026-03-18 08:38:50 -0300

    Update Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,5 +1,8 @@
 print('Vamos somar!')
+#Considera os valores solicitados
 v1 = int(input('Insira o primeiro valor: '))
 v2 = int(input('Insira o segundo valor: '))
+#Calcula a soma
 soma = (v1 + v2)
+#Exibe o resultado
 print('O resultado de' ,(v1), '+', (v2), 'é' ,(soma))
