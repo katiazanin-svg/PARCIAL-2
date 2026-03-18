73a24f47ca355091aac68e59ff7114e025b76a85
katiazanin-svg <katia.zanin@escola.pr.gov.br>
2026-03-18 08:40:31 -0300

    Update Desafio7.py

--- Desafio7.py
+++ Desafio7.py
@@ -1,6 +1,9 @@
 print ('Vamos calcular os juros simples!')
+#Considera os valores solicitados
 c = float(input('Insira o capital inicial:'))
 i = float(input('Insira a taxa:'))
 t = float(input('Insira o tempo:'))
+#Calcula o juros
 Juros = (c*i*t)
+#Exibe o resultado
 print ('Juros=',Juros)
