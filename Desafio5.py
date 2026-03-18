f604a57e20f802d72d84c55a3ab82350de1b7891
katiazanin-svg <katia.zanin@escola.pr.gov.br>
2026-03-18 08:45:05 -0300

    Create Desafio5.py

---
+++ Desafio5.py
@@ -0,0 +1,8 @@
+print ('Vamos calcular a área de um triângulo!')
+#Considera os valores solicitados
+b = float(input('Insira a medida(cm) da base:'))
+a = float(input('Insira a medida(cm) da altura:'))
+#Calcula a área
+Área = (b*a/2)
+#Exibe o resultado
+print ('Área =',Área)
