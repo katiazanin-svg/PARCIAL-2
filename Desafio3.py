4912c88d8c98164ab3a874a4c97d2b629adb1a26
katiazanin-svg <katia.zanin@escola.pr.gov.br>
2026-03-31 09:24:54 -0300

    Update Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,4 +1,11 @@
-#Lista os nomes
-Comandantes = 'Ramos','Weber','Henrique','Machado','Lucas'
+#Nomeia o código
+print ('Lista de Compras da Semana:')
+#Solicita cada item, indicando a posição da quantidade
+l1 = (input("Insira o item 1/5: "))
+l2 = (input("Insira o item 2/5: "))
+l3 = (input("Insira o item 3/5: "))
+l4 = (input("Insira o item 4/5: "))
+l5 = (input("Insira o item 5/5: "))
+Lista = l1, l2, l3, l4, l5
 #Exibe a lista
-print ('Os comandantes masculinos do 3º ano C são' , Comandantes)
+print ('A lista de compras para a semana é:' , Lista)
