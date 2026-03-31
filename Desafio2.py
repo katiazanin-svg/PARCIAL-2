9f02cabfe2d09a3b771b3d22330414aace5e6133
katiazanin-svg <katia.zanin@escola.pr.gov.br>
2026-03-31 09:06:33 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -3,5 +3,5 @@ print ('Vamos descobrir se um número é par ou ímpar?')
 n = int(input("Digite um número: ")) 
 #Veriica se é par
 if n % 2 == 0: print(f"O número {n} é par.") 
-#Se não for é ímpar
+#Se não for, é ímpar
 else: print(f"O número {n} é ímpar.")
